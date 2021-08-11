aa3f42f2d56c69883ef3e253e333794280c14267
scw <kaya1436@daum.net>
2021-08-11 13:50:49 +0900

    commit web

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from home import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.main_home),
+    path('main_home', views.main_home),
+    path('page01', views.page01),
 ]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+def main_home(request):
+    result={}
+    return render(request, 'main_home.html', context=result)
+
+def page01(request):
+    result={}
+    return render(request, 'page01.html', context=result)
