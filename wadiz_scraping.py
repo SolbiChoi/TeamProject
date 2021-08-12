8a4da5623be83120254c25ff5eedf113ca9cad4c
scw <kaya1436@daum.net>
2021-08-12 15:13:04 +0900

    스크랩

--- wadiz_scraping.py
+++ wadiz_scraping.py
@@ -9,14 +9,16 @@ driver.get('https://www.wadiz.kr/web/wreward/main?keyword=&endYn=Y&order=support
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 
-data_list = soup.select('div.ProjectCardList_list__1YBa2 > div > div > div')
-
+data_list = soup.select('div.ProjectCardList_list__1YBa2 > div > div > div > div')
 for data in data_list:
-    title = data.select('div.RewardProjectCard_infoTop__3QR5w > a > p > strong')[0].text.strip()
-    category = data.select('div.RewardProjectCard_infoTop__3QR5w > div > span.RewardProjectCard_category__2muXk')[0].text.strip()
+    title = data.select('strong')[0].text.strip()
+    category = data.select('span.RewardProjectCard_category__2muXk')[0].text.strip()
     score = data.select('span.RewardProjectCard_percent__3TW4_')[0].text.strip()
-
-    print(title, category,score)
-
+    print(title)
+    for i in range(len(data_list)):
+        time.sleep(0.5)
+        page = driver.find_element_by_css_selector('div.RewardProjectCard_infoTop__3QR5w > a')
+        page.click()
+        time.sleep(0.5)
 driver.close()
 driver.quit()
