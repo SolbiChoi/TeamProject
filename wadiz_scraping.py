b3ffbaec083c9a5aefffad3f9815e80cd0c78705
scw <kaya1436@daum.net>
2021-08-12 16:45:38 +0900

    스크롤

--- wadiz_scraping.py
+++ wadiz_scraping.py
@@ -1,24 +1,26 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
-import pandas as pd
 import requests
+from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome('/chromedriver.exe')
 driver.get('https://www.wadiz.kr/web/wreward/main?keyword=&endYn=Y&order=support')
+
+num_scroll = 15
+while num_scroll:
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight-1000);")
+    time.sleep(2)
+    num_scroll -= 1
+time.sleep(1)
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
-
 data_list = soup.select('div.ProjectCardList_list__1YBa2 > div > div > div > div')
+time.sleep(1)
 for data in data_list:
     title = data.select('strong')[0].text.strip()
     category = data.select('span.RewardProjectCard_category__2muXk')[0].text.strip()
     score = data.select('span.RewardProjectCard_percent__3TW4_')[0].text.strip()
-    print(title)
-    for i in range(len(data_list)):
-        time.sleep(0.5)
-        page = driver.find_element_by_css_selector('div.RewardProjectCard_infoTop__3QR5w > a')
-        page.click()
-        time.sleep(0.5)
+    print(title, category, score)
 driver.close()
 driver.quit()
