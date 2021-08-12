305dd85cd96165fac6a538693aa6bda618743295
scw <kaya1436@daum.net>
2021-08-12 17:49:18 +0900

    data

--- Scraping/wadiz_scraping.py
+++ Scraping/wadiz_scraping.py
@@ -1,26 +1,33 @@
+import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
 import requests
-from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome('/chromedriver.exe')
 driver.get('https://www.wadiz.kr/web/wreward/main?keyword=&endYn=Y&order=support')
 
+# 스크롤 15번 내림
 num_scroll = 15
 while num_scroll:
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight-1000);")
     time.sleep(2)
     num_scroll -= 1
 time.sleep(1)
+
+# 데이터 가져오기
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 data_list = soup.select('div.ProjectCardList_list__1YBa2 > div > div > div > div')
 time.sleep(1)
+funding_list = []
 for data in data_list:
     title = data.select('strong')[0].text.strip()
     category = data.select('span.RewardProjectCard_category__2muXk')[0].text.strip()
     score = data.select('span.RewardProjectCard_percent__3TW4_')[0].text.strip()
-    print(title, category, score)
+    funding_list.append([title, category, score])
+    columns = ['title', 'category', 'score']
+    result = pd.DataFrame(funding_list, columns=columns)
+result.to_excel('./files/data.xlsx', index=False)
 driver.close()
 driver.quit()
