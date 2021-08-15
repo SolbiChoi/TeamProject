08e11bff933c71dd38801ceb166b5c4e64f62aef
하영잉 <80766275+young-ha713@users.noreply.github.com>
2021-08-15 19:50:36 +0900

    db2

--- polls/models.py
+++ polls/models.py
@@ -1,8 +1,22 @@
 from django.db import models
 
 # Create your models here.
-class Question(models.Model):
-    question_text = models.CharField(max_length=100)
-    public_date = models.DateTimeField()
-    votes = models.BigIntegerField()
+class culture_scraping(models.Model):
+    stargrade= models.TextField()
+    review= models.TextField()
 
+class fashion_scraping(models.Model):
+    stargrade=models.TextField
+    review=models.TextField
+
+class food_scraping(models.Model):
+    stargrade=models.TextField
+    review=models.TextField
+
+class living_scraping(models.Model):
+    stargrade=models.TextField
+    review=models.TextField
+
+class tech_scraping(models.Model):
+    stargrade=models.TextField
+    review=models.TextField
