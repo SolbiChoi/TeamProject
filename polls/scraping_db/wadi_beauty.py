6849f16b18f356fd61b4bf52f0d21acaa0a7046a
하영잉 <80766275+young-ha713@users.noreply.github.com>
2021-08-18 09:28:20 +0900

    beauty_db

---
+++ polls/scraping_db/wadi_beauty.py
@@ -0,0 +1,10 @@
+import pandas as pd
+df = pd.read_excel('C:/Users/gkdud/PycharmProjects/TeamProject/Scraping/files/beauty_scraping.xlsx')
+
+import sqlite3
+connect = sqlite3.connect('./wadizdb.sqlite3')
+df.to_sql('table_beauty', connect, if_exists='append', index=False )
+
+
+
+connect.close()
